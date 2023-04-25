a7047cf2b0a99f478d50df06ee70615837c45664
snow <mlafuente98@gmail.com>
2023-04-26 01:33:48 +0200

    quelle galère, les sections du portfolio marchent il faut faire attention, les images de la grid se comportent bizarrement depuis, le object fit cover ne fonctionne plus vraiment, les sections sont devenues de la même couleur que le background

--- Java.js
+++ Java.js
@@ -1,24 +1,44 @@
 function toggleCategory(categoryId) {
   var categoryContent = document.getElementById(categoryId + '-content');
-  if (categoryContent.style.display === 'none') {
-    categoryContent.style.display = 'grid';
+  if (categoryContent.classList.contains('category-open')) {
+    categoryContent.classList.remove('category-open');
+    categoryContent.style.maxHeight = '0px';
   } else {
-    categoryContent.style.display = 'none' ;
+    categoryContent.classList.add('category-open');
+    categoryContent.style.maxHeight = categoryContent.scrollHeight + 'px';
   }
 }
 
-document.getElementById('argentique').onclick = function (){
-  toggleCategory ('argentique');
+document.getElementById('argentique').onclick=function (){
+  toggleCategory('argentique');
 }
 
-document.getElementById('danse').onclick = function (){
-  toggleCategory ('danse');
+document.getElementById('danse').onclick=function (){
+  toggleCategory('danse');
 }
 
-document.getElementById('portrait').onclick = function (){
-  toggleCategory ('portrait');
+document.getElementById('portrait').onclick=function (){
+  toggleCategory('portrait');
 }
 
-document.getElementById('ancolie').onclick = function (){
-  toggleCategory ('ancolie');
+document.getElementById('ancolie').onclick=function (){
+  toggleCategory('ancolie');
 }
+
+const categoryBtns = document.querySelectorAll('.category-btn');
+categoryBtns.forEach(function(categoryBtn) {
+  categoryBtn.addEventListener('click', function() {
+    const categoryContent = this.nextElementSibling;
+    if (categoryContent.style.maxHeight) {
+      categoryContent.style.maxHeight = null;
+    } else {
+      categoryContent.style.maxHeight = categoryContent.scrollHeight + 'px';
+    }
+  });
+});
+
+const photoArgentique = document.querySelector('.photo-argentique');
+photoArgentique.style.display = 'grid';
+
+const photoDanse = document.querySelector('.photo-danse');
+photoArgentique.style.display = 'grid';
